60d7de5f211babc7f6d739ff4a4180ee93387af3
Carine Tang <carinetang20@gmail.com>
2026-09-05 11:03:24 +0800

    Main page

--- app/src/main/java/com/example/hackathon/ReportActivity.java
+++ app/src/main/java/com/example/hackathon/ReportActivity.java
@@ -1,6 +1,8 @@
 package com.example.hackathon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -26,6 +28,30 @@ public class ReportActivity extends AppCompatActivity {
         issueSpinner = findViewById(R.id.issueSpinner);
         submitButton = findViewById(R.id.submitButton);
 
+        String[] issueTypes = {
+                "Illegal Parking",
+                "Pothole",
+                "Construction",
+                "Overgrown Vegetation",
+                "Broken Ramp",
+                "Blocked Tactile Path",
+                "Broken Crossing",
+                "Open Drain",
+                "Other"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                issueTypes
+        );
+
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        issueSpinner.setAdapter(adapter);
+
         submitButton.setOnClickListener(v -> {
 
             String location = locationInput.getText().toString();
@@ -33,6 +59,7 @@ public class ReportActivity extends AppCompatActivity {
             String issueType = issueSpinner.getSelectedItem().toString();
 
             if (location.isEmpty() || description.isEmpty()) {
+
                 Toast.makeText(
                         ReportActivity.this,
                         "Please fill in all fields",
@@ -47,6 +74,15 @@ public class ReportActivity extends AppCompatActivity {
                     "Report submitted!",
                     Toast.LENGTH_SHORT
             ).show();
+
+            // Go back to MainActivity
+            Intent intent = new Intent(
+                    ReportActivity.this,
+                    MainActivity.class
+            );
+
+            startActivity(intent);
+            finish();
         });
     }
 }
